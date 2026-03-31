5b91e29f2050636dc15b40e2a75d7f02358c0fc2
teya1018 <altheajanecordova0@gmail.com>
2026-03-31 11:38:42 +0800

    Initial commit

---
+++ seq_101.v
@@ -0,0 +1,74 @@
+/*============================================
+                SEQUENCE 101
+==============================================
+Description:
+ This is Moore FSM detects the 
+non-overlapping sequence 101, 
+assuming the input stream arrives LSB-first.
+
+Design Engineer:
+ Althea Jane E. Cordova 
+
+Date:
+ 31 March 2026
+-----------------------------------------------*/
+module seq_101(out, state, in, clk, rst_n);
+ // ports 
+ input 		 in;
+ input 		 clk;
+ input 		 rst_n;
+ output reg      out;
+ output reg[1:0] state;
+ 
+ // state assignment 
+ parameter [1:0] S0 = 2'b00;
+ parameter [1:0] S1 = 2'b01;
+ parameter [1:0] S2 = 2'b10;
+ parameter [1:0] S3 = 2'b11;
+ reg [1:0] nxt;
+ reg [1:0] pre;
+ 
+ // input block 
+ always @(in,pre)begin
+    case(pre)
+       S0: nxt = in? S1:S0;
+		 S1: nxt = in? S1:S2;
+		 S2: nxt = in? S3:S0;
+		 S3: nxt = in? S1:S0;
+		 default: nxt = S0;
+	 endcase	 
+ end 
+ 
+ // sequential block 
+ always @(posedge clk,negedge rst_n)begin
+	if(!rst_n) pre <= S0;
+	else 		  pre <= nxt;
+ end
+ 
+ // output block
+ always @(pre)begin
+	case(pre)
+		S0: begin 
+		      out = 0;
+	         state = S0;
+           end
+		S1: begin 
+            out = 0;
+		      state = S1;
+          end
+		S2: begin 
+		      out = 0;
+            state = S2;
+   		  end 
+		S3: begin 
+            out = 1;
+            state = S3;
+		    end 
+		default: begin 
+            out = 0;
+            state = S0;
+               end 
+                  
+	endcase
+ end
+endmodule
---
+++ testbench.v
@@ -0,0 +1,32 @@
+ // testbench for seq_101
+module testbench;
+ // tb signals 
+  reg  in;
+  reg  clk;
+  reg  rst_n;
+  wire out;
+  wire [1:0] state;
+ 
+ 
+ // instantiate seq_101
+  seq_101 dut (
+    .out   (out),
+    .state (state),
+    .in    (in),
+    .clk   (clk),
+    .rst_n (rst_n)
+  );
+ 
+
+ // apply stimuli
+  initial clk = 0;
+  always #1 clk = ~clk;
+  
+  initial begin
+    rst_n = 0; in = 1; #6;
+	 rst_n = 1; in = 1; #2;
+	 rst_n = 1; in = 0; #2;
+	 rst_n = 1; in = 1; #2;
+	 rst_n = 1; in = 0; #2;
+  end
+endmodule 
