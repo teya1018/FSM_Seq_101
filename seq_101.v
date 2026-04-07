1cf56da705d78b24e371ff77415da8e76f5019b5
teya1018 <altheajanecordova0@gmail.com>
2026-04-07 08:58:41 +0800

    initial

--- seq_101.v
+++ seq_101.v
@@ -20,6 +20,9 @@ module seq_101(out, state, in, clk, rst_n);
  output reg      out;
  output reg[1:0] state;
  
+ // clock 3s period
+ 
+ 
  // state assignment 
  parameter [1:0] S0 = 2'b00;
  parameter [1:0] S1 = 2'b01;
